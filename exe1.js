342e41f7f688430df6ab856e378300905f94d7eb
micro <microfalowka@gmail.com>
2023-04-26 15:26:34 +0200

    tomongo

--- exe1.js
+++ exe1.js
@@ -1,8 +1,10 @@
 
 
-const mongo = require("mongodb");
+// const mongo = require("mongodb");
+const {MongoClient, ObjectId} = require("mongodb");
 
-const client = new mongo.MongoClient("mongodb://127.0.0.1:27017");
+// const client = new mongo.MongoClient("mongodb://127.0.0.1:27017");
+const client = new MongoClient("mongodb://127.0.0.1:27017");
 
 (async()=>{
 
@@ -32,7 +34,7 @@ const client = new mongo.MongoClient("mongodb://127.0.0.1:27017");
     for await(const song of db.collection('songs2').find({
         artist : 'debil',
         })){
-            console.log(song)
+            // console.log(song)
         }
 
     const email = {$ne: ''} // injection notexist : ''
@@ -43,6 +45,12 @@ const client = new mongo.MongoClient("mongodb://127.0.0.1:27017");
             console.log(song)
          }  
 
+    const oneUser = await db.collection("songs2").findOne({
+        // _id: mongo.ObjectId('6448e5056b38a6c38f91cce5')  // nie trzeba bo zimportowane/wyciagniete ObjectId
+        _id: new ObjectId('6448e5056b38a6c38f91cce5')
+
+    })
+    console.log('wybdziubane z requirea' ,oneUser)
 
     await client.close();
 })()
---
+++ exe2_toMongo/utils/db2.js
@@ -0,0 +1,22 @@
+// const mysql = require('mysql2/promise');
+
+// const poolz = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'todo_data',
+//     // decimalNumber: true ,
+//     namedPlaceholders: true,
+    
+// })
+
+// module.exports = {
+//     poolz,
+// }
+
+
+// (async()=> {
+   
+//     await poolz.end();
+
+
+// })();
