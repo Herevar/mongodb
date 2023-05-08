e2fca83f69c3dfcd6810348561f3753eb259cda3
micro <microfalowka@gmail.com>
2023-05-08 19:39:53 +0200

    instrukcja

--- startowa.js
+++ startowa.js
@@ -28,12 +28,6 @@ const client = new mongo.MongoClient("mongodb://127.0.0.1:27017") ;
 
 
 
-
-
-
-
-
-
 // ----------------------
 
 // const mysql = require('mysql2/promise');
@@ -50,3 +44,18 @@ const client = new mongo.MongoClient("mongodb://127.0.0.1:27017") ;
 // module.exports = {
 //     poolz,
 // }
+
+
+
+
+// Krótka instrukcja dla tych którzy chcą to ogarniać bez myślenia:
+// 1. https://www.mongodb.com/try/download/community - pobierzcie
+// 2. https://www.mongodb.com/try/download/shell - pobierzcie
+// 3. Zainstalujcie mongodb z odznaczeniem tego serwisu jak kuba pokazywał na filmie, a tego mongo shella wrzućcie gdzie tam chcecie, ja wrzuciłem do folderu MongoDB tam gdzie się zainstalował.
+// 4. (prawdopodobnie opcjonalny) Zgodnie z tym linkiem https://www.mongodb.com/docs/mongodb-shell/install/ zróbcie to co wam poleca w punkcie 4. czyli dodajcie sobie w tym systemie ścieżki do mongod.exe i mongosh.exe oczywiście bez nich, czyli tam np. "C:\Program Files\MongoDB\Server\6.0\bin" dla mongod i "C:\Program Files\MongoDB\mongosh\bin" dla mongosh.
+// 5. Teraz utwórzcie sobie na dysku C folder "data" i w nim podfolder "db".
+// 6. Od tej chwili możecie odpalić mongod.exe i to startuje serwer. A po odpaleniu mongosh.exe macie nakładkę jakby do sterowania tym serwerem.
+// 7. Na koniec możecie sobie dodać do WebStorma skrypty na odpalanie tego:
+// "start-mongod": "cd /d C:\\Program Files\\MongoDB\\Server\\6.0\\bin && mongod.exe",
+// "start-mongosh": "cd /d C:\\Program Files\\MongoDB\\mongosh\\bin && mongosh.exe"
+// Oczywiście ścieżki sobie sami tam ustawcie albo zainstalujcie jak ja to kopiujcie
